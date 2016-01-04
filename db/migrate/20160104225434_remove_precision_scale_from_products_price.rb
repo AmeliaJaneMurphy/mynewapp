45f1c33b33bdbe61be01c4f2e5f40f2be67ded91
Amelia Jane <ameliajanemurph@gmail.com>
2016-01-04 15:01:55 -0800

    products price to decial and added dollar sign to html

---
+++ db/migrate/20160104225434_remove_precision_scale_from_products_price.rb
@@ -0,0 +1,5 @@
+class RemovePrecisionScaleFromProductsPrice < ActiveRecord::Migration
+  def change
+  	  	change_column :products, :price, :decimal
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160104224145) do
+ActiveRecord::Schema.define(version: 20160104225434) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.string   "image_url"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "color"
-    t.integer  "price"
+    t.decimal  "price",       precision: 6, scale: 3
   end
 
 end
