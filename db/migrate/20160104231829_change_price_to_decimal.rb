e984d03ba9c0c7490a1d10c328114f79e6974b93
Amelia Jane <ameliajanemurph@gmail.com>
2016-01-05 18:13:43 -0800

    modifying product price migrate to incorporate postgre language for heroku

--- db/migrate/20160104231829_change_price_to_decimal.rb
+++ db/migrate/20160104231829_change_price_to_decimal.rb
@@ -1,5 +1,5 @@
 class ChangePriceToDecimal < ActiveRecord::Migration
   def change
-  	change_column :products, :price, :decimal
+  	change_column :products, :price, 'decimal USING CAST(price AS decimal)'
   end
 end
