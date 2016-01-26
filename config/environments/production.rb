78c0d41c123e341e082a377f265fb078691f1e56
Amelia Jane <ameliajanemurph@gmail.com>
2016-01-26 13:30:37 -0800

    actionmailer test 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,13 +78,12 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 end
 
-#Sendgrid
 ActionMailer::Base.smtp_settings = {
-  :address => 'stmp.sendgrid.net',
+  :address => 'smtp.sendgrid.net',
   :port => '587',
   :authentication => :plain,
-  :user_name => ENV['ameliamurphy'],
-  :password => ENV['audrey85'],
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
   :domain => 'heroku.com',
   :enable_starttls_auto => true
 }
